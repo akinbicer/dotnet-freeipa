1b3164e401d02ae8949bef2877301724345edb66
Akın BİÇER <akin.bicer@outlook.com.tr>
2023-07-08 16:25:11 +0300

    - Added user actions. - Added custom transaction messages.

--- FreeIPA.DotNet/Models/RPC/IpaRpcResponseModel.cs
+++ FreeIPA.DotNet/Models/RPC/IpaRpcResponseModel.cs
@@ -5,12 +5,8 @@ namespace FreeIPA.DotNet.Models.RPC;
 public class IpaRpcResponseModel
 {
     [JsonProperty("result")] public object? Result { get; set; }
-
     [JsonProperty("error")] public object? Error { get; set; }
-
     [JsonProperty("id")] public int Id { get; set; }
-
     [JsonProperty("principal")] public string? Principal { get; set; }
-
     [JsonProperty("version")] public string? Version { get; set; }
 }
