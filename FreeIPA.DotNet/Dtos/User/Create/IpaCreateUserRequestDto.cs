33438486ea831f16b153f02e6da3471a31f23911
Akın BİÇER <akin.bicer@outlook.com.tr>
2023-08-06 22:44:24 +0300

    Updated ipa-client codes...

--- FreeIPA.DotNet/Dtos/User/Create/IpaCreateUserRequestDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FreeIPA.DotNet.Dtos.User.Create;
-
-public class IpaCreateUserRequestDto
-{
-    public required string FirstName { get; set; }
-    public required string LastName { get; set; }
-    public required string Username { get; set; }
-    public required string Password { get; set; }
-}
--- FreeIPA.DotNet/Dtos/User/Find/IpaFindUserRequestDto.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace FreeIPA.DotNet.Dtos.User.Find;
-
-public class IpaFindUserRequestDto
-{
-    public string? Username { get; set; }
-}
